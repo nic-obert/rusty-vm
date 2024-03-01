e8f32a8ade5ab3ec521e94a30d05a4e309525c1c
Nicholas Obert <nchlsuba@gmail.com>
2024-03-01 20:21:46 +0100

    updated count_lines.sh to show the lines of each project separately

--- count_lines.sh
+++ count_lines.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+echo
+
 echo "Rust:"
-find . -type f -name "*.rs" ! -wholename "**/target/*" | xargs wc -l | sort -nr
+find . -type f -name "*.rs" ! -wholename "**/target/*" | xargs wc -l | sort -nr | head -n 1
+
+echo $'\nVM:'
+find ./vm -type f -name "*.rs" | xargs wc -l | sort -nr
+
+echo $'\nAssembler:'
+find ./assembler -type f -name "*.rs" | xargs wc -l | sort -nr
+
+echo $'\nOxide:'
+find ./oxide -type f -name "*.rs" | xargs wc -l | sort -nr
 
 echo $'\nAssembly:'
 find . -name "*.asm" | xargs wc -l | sort -nr
