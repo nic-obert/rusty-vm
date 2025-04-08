fe4308b7e44a1b787e5b722d0e77972f56f0d5db
Nicholas Obert <nchlsuba@gmail.com>
2025-04-08 10:09:49 +0200

    updated count lines script

--- count_lines.sh
+++ count_lines.sh
@@ -14,8 +14,16 @@ find ./assembler -type f -name "*.rs" | xargs wc -l | sort -nr
 echo $'\nOxide:'
 find ./oxide -type f -name "*.rs" | xargs wc -l | sort -nr
 
+echo $'\nDebugger:'
+find ./debugger -type f -name "*.rs" | xargs wc -l | sort -nr
+
+echo $'\ndebug info viewer:'
+find ./debug_info_viewer -type f -name "*.rs" | xargs wc -l | sort -nr
+
+echo $'\nShared lib:'
+find ./rusty_vm_lib -type f -name "*.rs" | xargs wc -l | sort -nr
+
 echo $'\nAssembly:'
 find . -name "*.asm" | xargs wc -l | sort -nr
 
 echo
-
